022e0f067597bde19fdf181c3f1605142fb8e629
Aditya Aloy Sen <aloysen951@gmail.com>
2018-08-08 15:49:30 +0530

    adding place to home screen

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,21 @@
 import * as firebase from 'firebase';
 require("firebase/firestore");
 const prodConfig = {
-  apiKey: "AIzaSyDwLMC9ij5E68i_HHSc5DJQCkL9wZk_LyA",
-  authDomain: "test-lab-f7e14.firebaseapp.com",
-  databaseURL: "https://test-lab-f7e14.firebaseio.com",
-  projectId: "test-lab-f7e14",
-  storageBucket: "test-lab-f7e14.appspot.com",
-  messagingSenderId: "112650045023"
+  apiKey: "AIzaSyDvrw3MpeJyFbRcU7gZs9o8nelq6I8ot8I",
+  authDomain: "radii-website.firebaseapp.com",
+  databaseURL: "https://radii-website.firebaseio.com",
+  projectId: "radii-website",
+  storageBucket: "radii-website.appspot.com",
+  messagingSenderId: "592618696557"
 };
 
 const devConfig = {
-  apiKey: "AIzaSyDwLMC9ij5E68i_HHSc5DJQCkL9wZk_LyA",
-    authDomain: "test-lab-f7e14.firebaseapp.com",
-    databaseURL: "https://test-lab-f7e14.firebaseio.com",
-    projectId: "test-lab-f7e14",
-    storageBucket: "test-lab-f7e14.appspot.com",
-    messagingSenderId: "112650045023"
+  apiKey: "AIzaSyDvrw3MpeJyFbRcU7gZs9o8nelq6I8ot8I",
+  authDomain: "radii-website.firebaseapp.com",
+  databaseURL: "https://radii-website.firebaseio.com",
+  projectId: "radii-website",
+  storageBucket: "radii-website.appspot.com",
+  messagingSenderId: "592618696557"
 };
 
 const config = process.env.NODE_ENV === 'production'
